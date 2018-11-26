af944b727346c80c5b1949066be015fb0ae94e10
Thomas Freund <tfreundc@gmail.com>
2018-11-26 20:17:40 +0200

    I created the cloudinary connection worked on the standard dashboard and made the api private

--- Gemfile
+++ Gemfile
@@ -30,3 +30,7 @@ group :development, :test do
   gem 'spring-watcher-listen', '~> 2.0.0'
   gem 'dotenv-rails'
 end
+gem 'carrierwave'
+
+
+gem 'cloudinary', '~> 1.9.1'
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def user_avatar(user)
+    if user.photo.present?
+      user.photo_url
+    else
+      image_path 'default_avatar.png'
+    end
+  end
 end
