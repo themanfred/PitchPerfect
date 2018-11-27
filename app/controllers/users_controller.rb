890d15534fd3c5e673961226bc5001cb8e550350
yoann benchetrit <benchetrityoann@gmail.com>
2018-11-27 14:28:02 +0200

    done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,7 @@
 class UsersController < ApplicationController
 
-  def dashboard
+  def profile
     @user = current_user
   end
 
-  def update
-    current_user.update(user_params)
-    redirect_to dashboard_path
-  end
-
-  private
-
-  def user_params
-    params.require(:user).permit(:last_name, :first_name, :description, :photo,:email)
-  end
 end
