7a7686233aa0fb3c6745bc620ef076c8fa51c63e
Thomas Freund <tfreundc@gmail.com>
2018-11-27 20:48:20 +0200

    finished the feedback

---
+++ app/controllers/feedbacks_controller.rb
@@ -0,0 +1,39 @@
+class FeedbacksController < ApplicationController
+  def show
+    @pitch = Pitch.find(params[:pitch_id])
+    @feedback = Feedback.find(params[:id])
+  end
+
+  def new
+    @pitch = Pitch.find(params[:pitch_id])
+    @feedback = Feedback.new
+  end
+
+  def create
+    @feedback = Feedback.new(feedback_params)
+    @pitch = Pitch.find(params[:pitch_id])
+    @pitch = @feedback.pitch
+    @feedback.user = current_user
+    if @feedback.save
+      redirect_to pitch_path(@pitch)
+    else
+      render :new
+    end
+  end
+
+  def update
+    @feedback = feedback.find(params[:id])
+    @feedback.update(feedback_params)
+
+    redirect_to pitch_feedback_path(@feedback)
+  end
+
+  private
+
+  def feedback_params
+    params.require(:feedback).permit(
+      :quality, :clarity, :pace, :body_language, :value, :language, :quality_comment, :clarity_comment, :pace_comment,
+      :body_language_comment, :value_comment, :language_comment, :comment
+    )
+  end
+end
--- app/models/pitch.rb
+++ app/models/pitch.rb
@@ -3,5 +3,4 @@ class Pitch < ApplicationRecord
   belongs_to :masterpitch, optional: true
 
   has_many :feedbacks
-  has_many :ratings, through: :feedbacks
 end
