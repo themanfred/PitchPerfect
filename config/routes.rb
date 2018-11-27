bbd98e68b5c59b1e8827e18437220e32d1707751
Gabriel Zerbib <gabrielzerbib6@gmail.com>
2018-11-27 13:28:10 +0200

    transparent navbar + sign in + log in

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   get :dashboard, to: 'users#dashboard'
   patch :dashboard, to: 'users#update'
-  resources :users
+  resources :users,
+  controllers: {
+        registration: 'users/registrations'
+      }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
