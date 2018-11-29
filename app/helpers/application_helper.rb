550bb069763c6ed71db5d414abff8d741078cde6
Gabriel Zerbib <gabrielzerbib6@gmail.com>
2018-11-29 12:50:34 +0200

    profile

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,7 @@ module ApplicationHelper
     if user.photo.present?
       user.photo_url
     else
-      image_path 'default_avatar.png'
+      image_path "default_avatar.png"
     end
   end
 end
